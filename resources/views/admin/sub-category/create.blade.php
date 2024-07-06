78cedb72fc5107b2f831e6a6e8c99e200619a23f
farhadcse7 <fmfarhad23@gmail.com>
2024-07-07 04:32:46 +0600

    admin template mastering- ecom july 04

---
+++ resources/views/admin/sub-category/create.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.master')
+
+@section('body')
+    <h1>Create Category Page</h1>
+
+@endsection
+
---
+++ resources/views/admin/sub-category/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.master')
+
+@section('body')
+    <h1>Add Category Page</h1>
+
+@endsection
+
